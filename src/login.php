490335a372e4b799a59f3b38b734fcad74cf15fc
jeffresh <manuel.pallanunez@alum.uca.es>
2021-03-28 10:17:25 +0200

    :construction:  add login with coockies and session

--- src/login.php
+++ src/login.php
@@ -1,3 +1,28 @@
 <?php
-  echo "Logeao"
+  session_start();
+
+  $mensaje = "";
+
+  if(isset($_SESSION['email'])) {
+
+    $id = $_SESSION['email'];
+    $pwd = $_SESSION['password'];
+    echo $_POST['recordar'];
+
+    if(isset($_POST['recordar'])) {
+      $vencimiento = time() + (30 * 24 * 3600);
+      setcookie('email', $id, $vencimiento);
+      setcookie('password', $pwd, $vencimiento);
+
+      $mensaje = "Conexion automatica activada";
+    } elseif (isset($_POST['recordar'])) {
+      setcookie('email');
+      setcookie('password');
+      $mensaje = 'Conexion automatica desactivada';
+    }
+  } else {
+    header('Location:login.html');
+    exit;
+    
+  }
 ?>
