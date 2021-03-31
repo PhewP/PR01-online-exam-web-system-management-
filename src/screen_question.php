3d6833112fdb2352e58f669635c260c2099287e2
jeffresh <manuel.pallanunez@alum.uca.es>
2021-03-31 23:01:25 +0200

    :sparkles: add screen question

---
+++ src/screen_question.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Crear Examen</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+        <script src="https://kit.fontawesome.com/ffaee44ffc.js" crossorigin="anonymous"></script>
+      </head>
+      <body>
+      <div class="container has-text-centered">
+          <div class="column is-8 is-offset-2">
+            <div class="box">
+                <h1 class="title">Crear Pregunta</h1>
+        <form action="crear_examen.php" >
+        <div class="field">
+            <label class="label">Enunciado</label>
+            <div class="control">
+            <textarea class="textarea" placeholder="Escriba el enunciado de la pregunta" name="enunciado"></textarea>
+            </div>
+        </div>
+        <div class="field">
+            <label class="label">Respuesta A</label>
+            <div class="control">
+                <input class="input" type="text" placeholder="Inserte una respuesta" name="respuestaA" required>
+            </div>
+        </div>
+        <div class="field">
+            <label class="label">Respuesta B</label>
+            <div class="control">
+                <input class="input" type="text" placeholder="Inserte una respuesta" name="respuestaB" required>
+            </div>
+        </div>
+        <div class="field">
+            <label class="label">Respuesta C</label>
+            <div class="control">
+                <input class="input" type="text" placeholder="Inserte una respuesta" name="respuestaC" required>
+            </div>
+        </div>
+        <div class="field">
+            <label class="label">Respuesta D</label>
+            <div class="control">
+                <input class="input" type="text" placeholder="Inserte una respuesta" name="respuestaD" required>
+            </div>
+        </div>
+
+        <div class="field">
+        <label class="label">Respuesta Correcta</label>
+        <div class="control">
+            <div class="select">
+            <select name="respuestaCorrecta" class="has-text-centered" required>
+                <option disabled>Seleccione una opción</option>
+                <option>A</option>
+                <option>B</option>
+                <option>C</option>
+                <option>D</option>
+            </select>
+            </div>
+        </div>
+        </div>
+
+        <div class="field is-grouped">
+        <div class="control">
+            <button class="button is-link">Submit</button>
+        </div>
+        </div>
+        
+        </form>
+        <div class="control">
+            <button class="button is-link is-light">Atrás</button>
+        </div>
+        </div>
+      </div>
+      </div>
+    </body>
+</html>
