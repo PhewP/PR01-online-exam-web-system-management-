96b6b6be15eb2eda654e718d45bafaac7e91f771
jeffresh <manuel.pallanunez@alum.uca.es>
2021-03-31 21:09:14 +0200

    :construct: add navbar

---
+++ src/components/navbar.php
@@ -0,0 +1,20 @@
+<?php
+function Navbar() {
+    print('<nav class="navbar" role="navigation" aria-label="main navigation">');
+    print('<div class="navbar-brand">');
+    print('<a class="navbar-item" href="https://esingenieria.uca.es/">');
+    print('<img src="https://exterior.fe.ccoo.es/a544ca1d3464b59edde9527d0ea5b6ac000063.png" width="50" height="500">');
+    print('</a>');
+    print('</div>');
+    print('<div id="navbarBasicExample" class="navbar-menu">');
+    print('<div class="navbar-end">');
+    print('<div class="navbar-item">');
+    print('<a class="button is-primary">');
+    print('<strong>Log out</strong>');
+    print('</a>');
+    print('</div>');
+    print('</div>');
+    print('</div>');
+    print('</nav>');
+}
+?>
