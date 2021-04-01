ddba760b5844c9f1e420677e66d10abd0f2b1ada
jeffresh <manuel.pallanunez@alum.uca.es>
2021-04-01 16:27:12 +0200

    :recycle: reaadapt navbar as component

--- src/components/navbar.php
+++ src/components/navbar.php
@@ -1,25 +1,25 @@
 <?php
 function Navbar() {
-    print('<nav class="navbar" role="navigation" aria-label="main navigation">');
-    print('<div class="navbar-brand">');
-    print('<a class="navbar-item" href="https://esingenieria.uca.es/">');
-    print('<img src="https://exterior.fe.ccoo.es/a544ca1d3464b59edde9527d0ea5b6ac000063.png" width="50" height="500">');
-    print('</a>');
-    print('</div>');
-    print('<div id="navbarBasicExample" class="navbar-menu">');
-    print('<div class="navbar-start">');
-    print('<a href="screen_subjects.php" class="navbar-item">');
-    print('Asignaturas');
-    print('</a>');
-    print('</div>');
-    print('<div class="navbar-end">');
-    print('<div class="navbar-item">');
-    print('<a class="button is-primary">');
-    print('<strong>Log out</strong>');
-    print('</a>');
-    print('</div>');
-    print('</div>');
-    print('</div>');
-    print('</nav>');
+    return '<nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+    <a class="navbar-item" href="https://esingenieria.uca.es/">
+    <img src="https://exterior.fe.ccoo.es/a544ca1d3464b59edde9527d0ea5b6ac000063.png" width="50" height="500">
+    </a>
+    </div>
+    <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+    <a href="screen_subjects.php" class="navbar-item">
+    Asignaturas
+    </a>
+    </div>
+    <div class="navbar-end">
+    <div class="navbar-item">
+    <a class="button is-primary">
+    <strong>Log out</strong>
+    </a>
+    </div>
+    </div>
+    </div>
+    </nav>';
 }
 ?>
