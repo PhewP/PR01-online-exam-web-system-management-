10ce552de902f6e22d470e050b2eea05ba659360
hope <esperanza.canocanalejas@alum.uca.es>
2021-04-02 16:39:40 +0200

    :bug: improve DB

--- uca101.sql
+++ uca101.sql
@@ -58,9 +58,10 @@ INSERT INTO `asignatura` (`id`, `id_Grado`, `codigo`, `nombre`, `fechaCurso`) VA
 CREATE TABLE `examen` (
   `id` int(11) NOT NULL,
   `id_Usuario` int(11) NOT NULL,
-  `id_Informe` int(11) NOT NULL,
+  `id_Informe` int(11) DEFAULT NULL,
   `fecha_ini` datetime DEFAULT NULL,
-  `fecha_fin` datetime DEFAULT NULL
+  `fecha_fin` datetime DEFAULT NULL,
+  `nombre` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
