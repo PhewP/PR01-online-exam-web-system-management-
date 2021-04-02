e64b3de19f9aa13b948ae3f69962d3b5a2b9eea5
hope <esperanza.canocanalejas@alum.uca.es>
2021-04-03 00:12:47 +0200

    :construction: add some functions

--- src/api/Api.class.php
+++ src/api/Api.class.php
@@ -1,7 +1,7 @@
 <?php 
 
     include("User.class.php");
-    include("Question.class.php");
+    include("Exam.class.php");
 
     class Api {
         private static $conexion;
@@ -9,6 +9,10 @@
         private static $queryGetSubjects;
         private static $queryCreateQuestion;
         private static $queryCreateAnswer;
+        private static $queryGetActiveTests;
+        private static $queryGetNOTActiveTests;
+        private static $queryGetQuestions;
+        private static $queryGetPendingTests;
 
 
         public function __construct($host, $dbname, $user, $pass) {
@@ -22,6 +26,15 @@
                 inner join usuario us on us.id = ua.id_Usuario and us.id = :id");
                 self::$queryCreateQuestion = self::$conexion->prepare("INSERT INTO pregunta (id_tema, nombre) VALUES(:id_tema, :nombre)");
                 self::$queryCreateAnswer = self::$conexion->prepare("INSERT INTO respuesta(id_pregunta, nombre, verdadera) VALUES(:id_pregunta, :nombre, :verdadera)");
+                self::$queryGetActiveTests = self::$conexion->prepare("SELECT e1.* FROM examen e1 
+                INNER JOIN usuario u1 ON e1.id_Usuario = u1.id and u1.id = :id where NOW() BETWEEN fecha_ini and fecha_fin");
+                self::$queryGetNOTActiveTests = self::$conexion->prepare("SELECT e1.* FROM examen e1
+                INNER JOIN usuario u2 ON e1.id_Usuario = u2.id and u2.id = :id where fecha_fin <= NOW()");
+                self::$queryGetQuestions = self::$conexion->prepare("SELECT p.* FROM pregunta p
+                INNER JOIN  examenpregunta ep ON p.id = ep.id_Pregunta
+                INNER JOIN examen e ON ep.id_Examen = e.id and e.id = :id");
+                self::$queryGetPendingTests = self::$conexion->prepare("SELECT e1.* FROM examen e1 
+                INNER JOIN usuario u1 ON e1.id_Usuario = u1.id and u1.id = :id where fecha_fin >= NOW()");
             } catch(Exception $e) {
                 die("Error :".$e->getMessage());
             } 
@@ -79,6 +92,81 @@
             self::$queryCreateAnswer->execute();
             self::$queryCreateAnswer->closeCursor();
             return new Answer($nombre, $letra, $esCorrecta);
+        }
+
+        public function getTemasExamen($examenId)
+        {
+            self::$queryGetQuestions->execute(array('id' => $examenId));
+            $temas = [];
+            $repetido = true;
+
+            while($tema = self::$queryGetQuestions->fetch())
+            {
+                foreach($temas as $temaActual)
+                {
+                    if($tema == $temaActual)
+                        $repetido = false;
+                }
+                if($repetido)
+                    $temas[] = $tema;
+                
+                $repetido = true;
+            }
+            return $temas;
+        }
+
+        public function getPendingTests($userId, $subjectId)
+        {
+            self::$queryGetPendingTests->execute(array('id'=> $userId));
+            $numPreguntas = self::$queryGetUser->rowCount();
+            $tema = [];
+            $tests = [];
+
+            while($test = self::$queryGetPendingTests->fetch()) {
+                $testId = $test['id'];
+                self::$queryGetQuestions->execute(array('id' => $testId));
+                $tema = $this->getTemasExamen($testId);
+                $tests[$test['nombre']] = new Exam($testId, $test['id_Usuario'],
+                 $numPreguntas,  $subjectId, $test['nombre'], $tema, $test['descripcion'], $test['fecha_ini'], $test['fecha_fin']);
+           
+            }
+            return $tests;
+        }
+
+        public function getActiveTests($userId, $subjectId)
+        {
+            self::$queryGetActiveTests->execute(array('id'=> $userId));
+            $numPreguntas = self::$queryGetUser->rowCount();
+            $tema = [];
+            $tests = [];
+
+            while($test = self::$queryGetActiveTests->fetch()) {
+                $testId = $test['id'];
+                self::$queryGetQuestions->execute(array('id' => $testId));
+                $tema = $this->getTemasExamen($testId);
+                $tests[$test['nombre']] = new Exam($testId, $test['id_Usuario'],
+                 $numPreguntas,  $subjectId, $test['nombre'], $tema, $test['descripcion'], $test['fecha_ini'], $test['fecha_fin']);
+           
+            }
+            return $tests;
+        }
+
+        public function getNOTActiveTests($userId, $subjectId)
+        {
+            self::$queryGetNOTActiveTests->execute(array('id'=> $userId));
+            $numPreguntas = self::$queryGetUser->rowCount();
+            $tema = [];
+            $tests = [];
+
+            while($test = self::$queryGetNOTActiveTests->fetch()) {
+                $testId = $test['id'];
+                self::$queryGetQuestions->execute(array('id' => $testId));
+                $tema = $this->getTemasExamen($testId);
+                $tests[$test['nombre']] = new Exam($testId, $test['id_Usuario'],
+                 $numPreguntas,  $subjectId, $test['nombre'], $tema, $test['descripcion'], $test['fecha_ini'], $test['fecha_fin']);
+
+            }
+            return $tests;
         }
     }
 ?>
