a81bdaced0e608e054c0bb117b4e25fd2f0086b1
jeffresh <manuel.pallanunez@alum.uca.es>
2021-04-03 20:01:11 +0200

    :recycle: add screen exam api logic

--- src/screen_exam.php
+++ src/screen_exam.php
@@ -1,42 +1,54 @@
 <?php
 
-include("base.php");
+
+
+function examenCreado(){
+    ?>
+        <div class="field is-grouped">
+                <div class="control">
+                <a href="screen_exam.php" class="button is-link">Crear Otro</a> 
+    
+                </div>
+                <a href="screen_teacher.php" class="button is-link is-light">Atrás</a> 
+        </div>
+    <?php
+}
 
 function CrearExamen() {
-?>
-    <form action="crear_examen.php" >
+    ?>
+    <form action="screen_exam.php" method="POST">
     <div class="field">
         <label class="label">Titulo</label>
         <div class="control">
-            <input class="input" type="text" placeholder="Inserte un título" required>
+            <input name="titulo" class="input" type="text" placeholder="Inserte un título" required>
         </div>
     </div>
 
     <div class="field">
         <label class="label">Número de preguntas</label>
         <div class="control">
-            <input class="input" type="number" placeholder="Inserte un número mayor que 5" min="5" step="1" required>
+            <input name="numeroPreguntas" class="input" type="number" placeholder="Inserte un número mayor que 5" min="5" step="1" required>
         </div>
     </div>
 
     <div class="field">
         <label class="label">Fecha </label>
         <div class="control">
-            <input class="input" type="date" name="fechaInicio" min="'.getDatetimeNow().'" required>
+            <input name="fecha" class="input" type="date" name="fechaInicio" min="'.getDatetimeNow().'" required>
         </div>
     </div>
 
     <div class="field">
         <label class="label">Hora Inicio</label>
         <div class="control">
-            <input class="input" type="time" name="horaInicio" required>
+            <input name="horaInicio" class="input" type="time" name="horaInicio" required>
         </div>
     </div>
 
     <div class="field">
         <label class="label">Hora Fin</label>
         <div class="control">
-            <input class="input" type="time" name="horaFin" required>
+            <input name="horaFin" class="input" type="time" name="horaFin" required>
         </div>
     </div>
 
@@ -44,11 +56,11 @@ function CrearExamen() {
     <label class="label">Temas</label>
     <div class="control">
         <div class="select is-multiple">
-        <select name="temas" multiple required>
+        <select name="temas[]" multiple required>
             <option disabled>Seleccione un o varios temas</option>
-            <option>Tema 1</option>
-            <option>Tema 2</option>
-            <option>Todos</option>
+            <option value="1">Tema 1</option>
+            <option value="2">Tema 2</option>
+            <option value="all">Todos</option>
         </select>
         </div>
     </div>
@@ -56,20 +68,55 @@ function CrearExamen() {
     <div class="field">
     <label class="label">Descripción</label>
     <div class="control">
-        <textarea class="textarea" placeholder="Escriba una descripción para los alumnos"></textarea>
+        <textarea name="descripcion" class="textarea" placeholder="Escriba una descripción para los alumnos"></textarea>
     </div>
     </div>
 
     <div class="field is-grouped">
     <div class="control">
-        <input type="submit" class="button is-link" value="Crear">
+        <input name="crear" type="submit" class="button is-link" value="Crear">
     </div>
-    <a href="login.php" class="button is-link is-light">Atrás</a> 
+    <a href="screen_teacher.php" class="button is-link is-light">Atrás</a> 
     </div>
     </form>
 <?php
 }
+    include("base.php");
+    session_start();
 
-Base("Crear Examen", 'CrearExamen');
+    $env = parse_ini_file("../.env");
+
+    $api = new Api($env['DB_HOST'], $env['DB_NAME'], $env['DB_USER'], $env['DB_PASSWORD']);
+
+    if(isset($_POST['crear'])) {
+
+        $idSubject = $_SESSION['asignaturaActual'];
+        $user =  unserialize($_SESSION['user']);
+        $userId = $user->getId();
+        //Hacer query de crear examen
+        //Mostrar crear otro o atras
+        echo "Examen creado";
+        echo "Asignatura: ".$idSubject;
+        echo "user: ".$user;
+
+        $nombre = $_POST['titulo'];
+        $numPreguntas = $_POST['numeroPreguntas'];
+        $fecha = $_POST['fecha'];
+        $fecha_ini = $fecha." ".$_POST['horaInicio']."</br>";
+        $fecha_fin = $fecha." ".$_POST['horaFin'];
+        $descripcion = $_POST['descripcion'];
+
+        echo $fecha_ini;
+        echo $fecha_fin;
+        //Tema es un array de temas
+        $temas= $_POST['temas'];
+   
+
+        $examen = $api->createExam($userId, $idSubject, $fecha_ini, $fecha_fin, $nombre, $descripcion, $temas, $numPreguntas);
+        Base("Examen creado con Exito", "examenCreado");
+    }
+    else { 
+        Base("Crear Examen", 'CrearExamen');
+    }
 
 ?>
