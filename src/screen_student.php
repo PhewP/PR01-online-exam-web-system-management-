0402fd7df40aae0d1e56179887fd8b18a2952675
hope <esperanza.canocanalejas@alum.uca.es>
2021-04-04 16:53:45 +0200

    :sparkles: add screen: Active Examen

--- src/screen_student.php
+++ src/screen_student.php
@@ -8,18 +8,66 @@
   
     $user = unserialize($_SESSION['user']);
 
-    $id = $_POST['subject'];
+    if(isset($_POST['subject'])){
+      $idSubject = $_POST['subject'];
+      $_SESSION['asignaturaActual'] = $idSubject;
+    }
+    else {
+      $idSubject = $_SESSION['asignaturaActual'];
+    }
 
     $env = parse_ini_file("../.env");
 
     $api = new Api($env['DB_HOST'], $env['DB_NAME'], $env['DB_USER'], $env['DB_PASSWORD']);
 
-    $exams = $api->getPendingTests($user->getId(), $id);
+    $exams = $api->getActiveTestsStudent($idSubject);
+
+    if($exams){
+      echo "<hr>";
+      ?>
+        <h1 class="title">Examenes Activos</h1> 
+      <?php
+    }
 
     foreach($exams as $exam)
     {
     ?>
-      <form action="screen_modify_exam.php" method="POST">
+      <form action="screen_active_exam.php" method="POST">
+        <div class="field">
+          <?php
+          if($api->getMark($user->getId(), $exam->getId()))
+          {
+          ?>
+            <button disabled class = "button is-link is-light"><?php echo $exam->getTitulo(); ?></button>
+            <?php
+          }else{
+          ?>
+            <input type = "hidden" name = "exam" value = "<?php echo $exam->getId(); ?>">
+            <button class = "button is-link is-light"><?php echo $exam->getTitulo(); ?></button>
+          <?php
+          }
+          ?>
+        </div>
+      </form>
+    <?php 
+      echo "<br>";
+    }
+
+    echo "<hr>";
+
+    $exams = $api->getNOTActiveTestsStudent($idSubject);
+
+    if($exams){
+      echo "<hr>";
+      ?>
+        <h1 class="title">Examenes Finalizados</h1> 
+      <?php
+    }
+    
+    foreach($exams as $exam)
+    {
+    ?>
+      <form action="screen_nota.php" method="POST">
         <div class="field">
           <input type = "hidden" name = "exam" value = "<?php echo $exam->getId(); ?>">
           <button class = "button is-link is-light"><?php echo $exam->getTitulo(); ?></button>
@@ -28,42 +76,8 @@
     <?php 
       echo "<br>";
     }
-
-    echo "<hr>";
-
-    $exams = $api->getActiveTests($user->getId(), $id);
-
-    foreach($exams as $exam)
-    {
-    ?>
-      <form>
-        <div class="field">
-          <button class = "button is-link is-light"><?php echo $exam->getTitulo(); ?></button>
-        </div>
-      </form>
-    <?php 
-      echo "<br>";
-    }
-
-    echo "<hr>";
-
-    $exams = $api->getNOTActiveTests($user->getId(), $id);
-    foreach($exams as $exam)
-  {
-  ?>
-    <form action="screen_inform.php" method="POST">
-      <div class="field">
-        <input type = "hidden" name = "exam" value = "<?php echo $exam->getId(); ?>">
-        <button class = "button is-link is-light"><?php echo $exam->getTitulo(); ?></button>
-      </div>
-    </form>
-  <?php 
-    echo "<br>";
-  }
     echo "<hr>"; 
-
 }
 
 Base("Estudiante", 'ExamenesActivos');
-
-?>
+    ?>
